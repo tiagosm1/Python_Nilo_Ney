bcc49b49fb6a21ec2af1e3056c3cfe58fa56dee3
TIAGO MOREIRA <72826762+tiagosm1@users.noreply.github.com>
2020-12-29 16:52:33 -0300

    1

--- ex3.7.py
+++ ex3.7.py
@@ -1,4 +1,10 @@
 n1 = int(input('Insira o primeiro número:'))
 n2 = int(input('Insira o segundo número:'))
 
+<<<<<<< Updated upstream
 print(n1 + n2) 
+=======
+soma = (n1 + n2)
+
+print(soma)
+>>>>>>> Stashed changes
