813b08082088f9b361fdde6817ea1ec2dd3f0b8f
tiagosm1 <72826762+tiagosm1@users.noreply.github.com>
2021-01-08 14:38:11 -0300

    5.5

---
+++ Feitos/cap4/SW5.1.py
@@ -0,0 +1,16 @@
+'''
+fim = int(input('Digite o último número a imprimir:'))
+x = 1
+while x <= fim:
+    print(x)
+    x = x + 1
+'''
+
+fim = int(input('Digite o último número a imprimir:'))
+x = 1
+while x <= fim:
+    if x % 2 == 0:
+        print(x)
+    x = x + 1
+
+
