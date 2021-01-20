3e3c966f2308c2a768590a5dc5d24c44d67383a9
tiagosm1 <72826762+tiagosm1@users.noreply.github.com>
2021-01-20 09:37:50 -0300

    sw5.3

---
+++ Feitos/cap5/SW5.3.py
@@ -0,0 +1,19 @@
+'''
+n = 1
+soma = 0
+while n <= 10:
+    x = int(input(f'Digite o {n} número:'))
+    soma = soma + x
+    n = n + 1
+print(f'Soma: {soma}')
+'''
+
+n = 1
+soma = 0
+while n <= 5:
+    x = int(input(f'Digite o {n}º número:'))
+    soma = soma + x
+    n = n + 1
+print(f'Média: {soma/5}')
+
+
