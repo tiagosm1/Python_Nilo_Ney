5a4211ac95bb92f2759c26f4b546bff4b89da5f5
tiagosm1 <72826762+tiagosm1@users.noreply.github.com>
2021-01-21 14:10:38 -0300

    Update ex9.3.py

--- Feitos/cap9/ex9.3.py
+++ Feitos/cap9/ex9.3.py
@@ -1,5 +1,33 @@
-with open('multiplos4.txt', 'r') as multiplos4:
-    with open('pares.txt') as pares:
-        for l in pares.readlines():
-            if int(l) % 4 == 0:
-                multiplos4.write(l)
+def lê_número(arquivo):
+    while True:
+        número = arquivo.readline()
+        # Verifica se conseguiu ler algo
+        if número == "":
+            return None
+        # Ignora linhas em branco
+        if número.strip() != "":
+            return int(número)
+
+
+def escreve_número(arquivo, n):
+    arquivo.write(f"{n}\n")
+
+
+pares = open("pares.txt", "r")
+ímpares = open("ímpares.txt", "r")
+pares_ímpares = open("pareseimpares.txt", "w")
+npar = lê_número(pares)
+nímpar = lê_número(ímpares)
+while True:
+    if npar is None and nímpar is None:  # Termina se ambos forem None
+        break
+    if npar is not None and (nímpar is None or npar <= nímpar):
+        escreve_número(pares_ímpares, npar)
+        npar = lê_número(pares)
+    if nímpar is not None and (npar is None or nímpar <= npar):
+        escreve_número(pares_ímpares, nímpar)
+        nímpar = lê_número(ímpares)
+
+pares_ímpares.close()
+pares.close()
+ímpares.close()
